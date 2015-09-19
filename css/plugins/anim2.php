8d31bd6cb67e362c7ea3df6f18f15c18f9e6d50f
Franku <sz.frankyy@gmail.com>
2015-09-19 11:41:58 +0200

    additional animations

--- css/plugins/anim2.php
+++ css/plugins/anim2.php
@@ -13,28 +13,25 @@ EOD;
 $animationDef=<<<EOD
 @keyframes anim {
   0% {
-        /*transform: scale(1,1);*/
+        transform: scale(1,1);
   }
   
   50%{
-     transform: translate(30px,-5px) scale(.8,.2); 
-     /*transform: translateX(30px);*/
-    /*transform: translateX(30px) scale(1,1);*/
+        transform: scaleY(600);
         
   }
   80% {
-    transform: translate(0px,5px)  scaleX(.8,.1) 
+    
     
   }
 
     90% {
-    transform: translate(0px,0px) scaleX(1,1);
     
   }
   
   
   100% {
-    transform: translate(-30px,10px) scale(.2,0);
+    /*transform: scale(.2,0);*/
     opacity: 0;
   }
 }
