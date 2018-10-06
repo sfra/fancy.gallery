b3e994d66be74d4168e9f19201eab1f673058898
Szymon Frankowski <sz.frankyy@gmail.com>
2018-10-06 12:49:23 +0200

    php dependency removed

--- php/getImgWrapper.php
+++ php/getImgWrapper.php
@@ -2,10 +2,8 @@
     function getImgWrapper($xdim, $ydim, $tile_width, $tile_height, $id, $img_name)
     {
         $out="<div id=\"$id\" class=\"fancy-gallery-img-wrapper fancy-gallery-clearfix\">";
-        $tile_height_perc = $tile_height/($ydim*$tile_height)*100;
-
-        $tile_width_perc = $tile_width/($xdim*$tile_width)*100;
-
+        
+        
 
         for ($i=0; $i< $ydim; $i++) {
             $out.= "<div>";
